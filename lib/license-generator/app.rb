2a5fce6b5177a71222af5eaaf8d46120a895e17e
Justin Blake <justin@hentzia.com>
2011-10-03 12:10:30 -0400

    Refactoring.

--- lib/license-generator/app.rb
+++ lib/license-generator/app.rb
@@ -11,11 +11,8 @@ module LicenseGenerator
     
     desc "list", "List available license templates"
     def list
-      Dir.foreach(self.class.source_root) do |template|
-        unless %w(. ..).include?(template)
-          template = template.split('.').first
-          say template 
-        end
+      for_each_template do |template|
+        say template
       end
     end
 
@@ -27,19 +24,26 @@ module LicenseGenerator
     def help(task = nil)
       super
       say "Templates:"
-      Dir.foreach(self.class.source_root) do |template|
-        unless %w(. ..).include?(template)
-          template = template.split('.').first
-          say "  lickjen #{template}\t# Generate a #{template} template"
-        end
+      for_each_template do |template|
+        say "  lickjen #{template}\t# Generate a #{template} template"
       end
     end
     
     private
+
     def option(name)
       @template_options ||= {}
       @template_options[name] ||= ask("#{name.capitalize}:")
       @template_options[name]
+    end
+    
+    def for_each_template
+      Dir.foreach(self.class.source_root) do |template|
+        unless %w(. ..).include?(template)
+          template = template.split('.').first
+          yield(template)
+        end
+      end
     end
   end
 end
