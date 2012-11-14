d7fe9ebb8e26560f3f930746f9e09a097349f4b2
Dayton Nolan <dnolan@gmail.com>
2012-11-13 23:48:27 -0600

    Rename lickjen everywhere

--- lib/license-generator/app.rb
+++ lib/license-generator/app.rb
@@ -3,12 +3,12 @@ require 'thor'
 module LicenseGenerator
   class App < Thor
     include Thor::Actions
-    
+
     # Path to the templates
     def self.source_root
       File.expand_path('../../templates', File.dirname(__FILE__))
     end
-    
+
     desc "list", "List available license templates"
     def list
       for_each_template do |template|
@@ -19,18 +19,18 @@ module LicenseGenerator
     def method_missing(meth, *args)
       template "#{meth}.erb", "LICENSE"
     end
-    
+
     # Override Thor#help to include templates.
     def help(task = nil)
       super
       unless task
         say "Templates:"
         for_each_template do |template|
-          say "  lickjen #{template}\t# Generate a #{template} template"
+          say "  licgen #{template}\t# Generate a #{template} template"
         end
       end
     end
-    
+
     private
 
     def option(name)
@@ -38,7 +38,7 @@ module LicenseGenerator
       @template_options[name] ||= ask("#{name.capitalize}:")
       @template_options[name]
     end
-    
+
     def for_each_template
       Dir.foreach(self.class.source_root) do |template|
         unless %w(. ..).include?(template)
