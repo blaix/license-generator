2ad3bd30c91bf4d8a66edca98caf34978524a007
Justin Blake <justin@hentzia.com>
2012-11-16 14:10:02 -0500

    Fix deprecations for nested cucumber steps.

--- features/step_definitions/license-generator_steps.rb
+++ features/step_definitions/license-generator_steps.rb
@@ -1,15 +1,17 @@
 Then /^the output should say the file "([^"]*)" was created$/ do |file|
-  Then "the output should match /create(.+)#{file}/"
+  step %{Then "the output should match /create(.+)#{file}/"}
 end
 
 Then /^the file "([^"]*)" should contain a copyright notice for "([^"]*)"$/ do |file, name|
   year = Time.now.year
-  Then "the file \"#{file}\" should contain \"Copyright (c) #{year}, #{name}\""
+  step %{Then "the file \"#{file}\" should contain \"Copyright (c) #{year}, #{name}\""}
 end
 
 Then /^I should see the generic help output$/ do
-  Then "the output should contain \"licgen help [TASK]\""
-  Then "the output should contain \"licgen list\""
-  Then "the output should contain \"licgen bsd\""
-  # TODO: add more conditions when we have more templates
+  steps %{
+    Then "the output should contain \"licgen help [TASK]\""
+    Then "the output should contain \"licgen list\""
+    Then "the output should contain \"licgen bsd\""
+    # TODO: add more conditions when we have more templates
+  }
 end
