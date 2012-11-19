1dcae37dca78ce59a09456f17faaf0e8b08f5560
Justin Blake <justin@hentzia.com>
2012-11-19 17:00:25 -0500

    Use aruba helpers instead of nested steps.
    Fixes some pending step issues for steps that should not have been
    pending.

--- features/step_definitions/license-generator_steps.rb
+++ features/step_definitions/license-generator_steps.rb
@@ -1,17 +1,15 @@
 Then /^the output should say the file "([^"]*)" was created$/ do |file|
-  step %{Then "the output should match /create(.+)#{file}/"}
+  steps %{Then "the output should match /create(.+)#{file}/"}
 end
 
 Then /^the file "([^"]*)" should contain a copyright notice for "([^"]*)"$/ do |file, name|
-  year = Time.now.year
-  step %{Then "the file \"#{file}\" should contain \"Copyright (c) #{year}, #{name}\""}
+  content = "Copyright (c) #{Time.now.year}, #{name}"
+  check_file_content(file, content, true)
 end
 
 Then /^I should see the generic help output$/ do
-  steps %{
-    Then "the output should contain \"licgen help [TASK]\""
-    Then "the output should contain \"licgen list\""
-    Then "the output should contain \"licgen bsd\""
-    # TODO: add more conditions when we have more templates
-  }
+  assert_partial_output("licgen help [TASK]", all_output)
+  assert_partial_output("licgen list", all_output)
+  assert_partial_output("licgen bsd", all_output)
+  assert_partial_output("licgen mit", all_output)
 end
