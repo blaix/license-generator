09b2edad116c39d9e25b02553fd1f51ab54cd510
Justin Blake <justin@hentzia.com>
2013-07-22 12:41:56 -0400

    Improve help output.
    Add a real usage example, and remove list of templates.
    
    We don't need to list available templates in the help output because we
    have a command for that, and it will clutter up help as the list grows.

--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -32,12 +32,6 @@ describe LicenseGenerator::App do
     @app.list
   end
 
-  it "displays the templates in the help command output" do
-    @app.should_receive(:say).with('Templates:')
-    @app.should_receive(:say).exactly(@app.templates.size).times.with(/^\s{2}licgen ([A-Za-z0-9\-\._]+)\b\s{1,}#\sGenerate\s([A-Za-z0-9\-\._]+)\slicense/)
-    @app.help
-  end
-
   it "generates a template with a valid template name" do
     @app.should_receive(:template).with('mit.erb', 'LICENSE')
     @app.mit('Justin Blake')
